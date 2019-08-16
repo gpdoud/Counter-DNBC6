b5897eed0eb77beed095a09086617d3e7c277afd
Greg Doud <gpdoud@gmail.com>
2019-08-16 15:23:13 -0400

    Final change

--- js/counter.js
+++ js/counter.js
@@ -16,7 +16,7 @@ function increment() {
 }
 
 function display() {
-    var ctrl = document.getElementById("theNbr")
+    var ctrl = document.getElementById("theNbr");
     ctrl.value = nbr;
     ctrl.style.fontWeight = nbr % 2 == 0 ? "bold" : "normal";
     ctrl.style.fontStyle = nbr % 3 == 0 ? "italic" : "normal";
